87170f68f239e109e12253900208bc13ba9832c4
Andrii Halenda <ahalenda@intellias.com>
2020-08-18 20:44:43 +0300

    few conf

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { VendorModule } from './vendor/vendor.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017' || process.env.MONGO_DB),
     VendorModule,
     ProductModule,
   ],
