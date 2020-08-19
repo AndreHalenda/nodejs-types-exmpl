f01118e28021340204d11bd98172b4270959312b
Andrii Halenda <ahalenda@intellias.com>
2020-08-19 13:33:21 +0300

    endpoints

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Get } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): string[] {
+    return [
+      '/vendors/all, Get',
+      '/vendors/:id, GET',
+      '/vendors/new-vendor, POST',
+      '/vendors/:id, PUT',
+      '/vendors/:id, DELETE',
+      '/products/all, GET',
+      '/products/:id, GET',
+      '/products/new-product',
+      '/products/:id, PUT',
+      '/products/:id, DELETE',
+      '/products/name/:name, GET',
+      '/products/vendor/:vendor, GET',
+    ]
   }
 }
